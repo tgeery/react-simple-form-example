a53a73d9c40fbbbdef33df65d0208ef0a2b265d9
Travis <tpgeery@socal.rr.com>
2020-06-26 23:28:45 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class MyLabel extends Component {
+    render() {
+        const val = this.props.mylabel;
+        return (
+            <h1>{val}</h1>
+        );
+    }
+}
+
+class MyText extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({mytext: e.target.value});
+    }
+
+    handleSubmit(e) {
+        alert("submit: " + this.state.mytext);
+        this.props.onTextSubmit(this.state.mytext);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="value" value={this.props.mytext} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+class MySubmit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mytext: ''
+        };
+        this.handleTextSubmit = this.handleTextSubmit.bind(this);
+    }
+
+    handleTextSubmit(text) {
+        alert("textSubmit: " + text);
+        this.setState({
+            mytext: text
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <MyText onTextSubmit={this.handleTextSubmit}/>
+                <MyLabel mylabel={this.state.mytext}/>
+            </div>
+        );
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MySubmit />
     </div>
   );
 }
